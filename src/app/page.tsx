c3cdf72393b16604c343ea6c54e0046892ae49fd
Roberto Júnior <juniorjp@gmail.com>
2024-06-11 16:08:06 -0300

    chore: add main app logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import logo from "/public/img/logo.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex flex-col min-h-screen items-center justify-center">
+      <Image src={logo} alt="Quack Visual!" />
       <span className="text-4xl font-bold">Quack!</span>
     </main>
   );
