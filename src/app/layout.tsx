95828a764a1a7828f00d6cab3a452a4b581b0615
Christian Leonardo <pleasefloyd73@gmail.com>
2024-06-17 11:55:20 -0300

    style: apply new lint and formats guides

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
-import type { Metadata } from "next"
+import type { Metadata } from 'next';
 
-import "@/styles/globals.css"
-import { cn } from "@/lib/utils"
-import { fontMono, fontSans } from "@/styles/fonts"
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from '@/components/theme-provider';
+
+import { cn } from '@/lib/utils';
+
+import { fontMono, fontSans } from '@/styles/fonts';
+import '@/styles/globals.css';
 
 export const metadata: Metadata = {
-  title: "Desafio do Pato",
-  description: "Plataforma de gestão de desafios"
-}
+	title: 'Desafio do Pato',
+	description: 'Plataforma de gestão de desafios',
+};
 
 export default function RootLayout({
-  children
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-BR" suppressHydrationWarning>
-      <body className={cn(fontSans, fontMono)}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  )
+	return (
+		<html lang='pt-BR' suppressHydrationWarning>
+			<body className={cn(fontSans, fontMono)}>
+				<ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-import { ThemeSwitcher } from "@/components/theme-switcher"
-import Image from "next/image"
-import logo from "/public/img/logo.png"
+import Image from 'next/image';
+
+import { ThemeSwitcher } from '@/components/theme-switcher';
+
+import logo from '/public/img/logo.png';
 
 export default function Home() {
-  return (
-    <main className="flex flex-col min-h-screen items-center justify-center">
-      <Image src={logo} alt="Quack Visual!" className="size-32 aspect-auto" />
-      <span className="text-4xl font-bold">Quack!</span>
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-center'>
+			<Image src={logo} alt='Quack Visual!' className='aspect-auto size-32' />
+			<span className='text-4xl font-bold'>Quack!</span>
 
-      <div className="absolute top-4 right-4">
-        <ThemeSwitcher />
-      </div>
-    </main>
-  )
+			<div className='absolute right-4 top-4'>
+				<ThemeSwitcher />
+			</div>
+		</main>
+	);
 }
