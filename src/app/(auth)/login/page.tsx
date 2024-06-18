1e46764fe7d14ce0fa870e3d008248e9c2451dda
Roberto Júnior <juniorjp@gmail.com>
2024-06-18 17:28:48 -0300

    feat: add auth folders and files for login

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Image from 'next/image';
+
+import { Typography } from '@/components/ui/typography';
+
+import AuthForm from '../_components/authForm';
+import logo from '/public/img/logo.png';
+
+export default function LoginPage() {
+  return (
+    <div className='flex w-full flex-col'>
+      <Typography.H2 className='mb-8 text-center'>Login</Typography.H2>
+      <AuthForm />
+    </div>
+  );
+}
