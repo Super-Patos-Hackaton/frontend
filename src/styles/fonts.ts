e019704ea8a304bf3603347e2372ad1467021bba
Christian Leonardo <pleasefloyd73@gmail.com>
2024-06-18 17:53:26 -0300

    chore: add acme font for headings

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,5 +1,14 @@
+import { Acme } from 'next/font/google';
+
 import { GeistMono } from 'geist/font/mono';
 import { GeistSans } from 'geist/font/sans';
 
+const acme = Acme({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-acme',
+});
+
+export const fontAcme = acme.variable;
 export const fontSans = GeistSans.variable;
 export const fontMono = GeistMono.variable;
