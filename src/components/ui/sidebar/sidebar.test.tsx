f74b42d53bab676746ec23694eeca8ab606edf27
Christian Leonardo <pleasefloyd73@gmail.com>
2024-06-18 19:23:34 -0300

    test: apply tests in sidebar component

---
+++ src/components/ui/sidebar/sidebar.test.tsx
@@ -0,0 +1,51 @@
+import { NavLinksSidebar } from '@/components/nav-links-sidebar';
+
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { debug } from 'console';
+import { describe, expect, it } from 'vitest';
+
+import { Sidebar } from '.';
+
+describe('Sidebar Component', () => {
+  it('should render the Sidebar component', () => {
+    render(
+      <div>
+        <Sidebar navLinks={<button>Hello</button>} />
+      </div>
+    );
+
+    debug();
+
+    const logo = screen.getByAltText(/Desafios do Pato - Logo/i);
+    const title = screen.getByRole('heading', {
+      level: 1,
+      name: /Desafios do Pato/i,
+    });
+
+    expect(title).toBeInTheDocument();
+    expect(logo).toBeInTheDocument();
+  });
+
+  it('should collapse the sidebar when user clicks the toggle button', () => {
+    render(<Sidebar navLinks={<button>Hello</button>} />);
+
+    const button = screen.getByTestId('sidebar-toggle');
+    const container = screen.getByTestId('container');
+
+    fireEvent.click(button);
+
+    expect(container).toHaveClass('w-[76px]');
+  });
+
+  it('should open the sidebar when user clicks the toggle button', () => {
+    render(<Sidebar navLinks={<button>Hello</button>} />);
+
+    const button = screen.getByTestId('sidebar-toggle');
+    const container = screen.getByTestId('container');
+
+    fireEvent.click(button);
+
+    expect(container).toHaveClass('w-80');
+  });
+});
