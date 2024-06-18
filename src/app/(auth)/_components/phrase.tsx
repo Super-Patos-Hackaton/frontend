2ae95828dcd8dbd382d880192eff4258cd0fdd3f
Roberto Júnior <juniorjp@gmail.com>
2024-06-18 17:27:08 -0300

    feat: phrase for auth pages left column

---
+++ src/app/(auth)/_components/phrase.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+export const Phrase = () => {
+  const pathName = usePathname();
+
+  return (
+    <span className='max-w-[550px] text-balance text-center text-5xl font-black text-secondary'>
+      {pathName.includes('login')
+        ? 'Seja como um Pato. Calmo na superfície e remando loucamente debaixo d’água'
+        : 'Filho de Peixe, Peixinho é'}
+    </span>
+  );
+};
