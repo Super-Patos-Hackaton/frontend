74c8c6467ac931fd794812d4cd36fa79617da3ef
Roberto Júnior <juniorjp@gmail.com>
2024-06-21 18:07:26 -0300

    refactor: change interface name to same file name

--- src/components/user-level.tsx
+++ src/components/user-level.tsx
@@ -6,28 +6,32 @@ import { CircularProgress } from '@/components/ui/graphElements';
 
 import Widget from './ui/widget';
 
-interface ScoreProps {
-  nextLevel: number;
+interface UserLevelProps {
+  nextLevelPercentage: number;
   level: number;
 }
 
-const UserLevel = (scoreProps: ScoreProps) => {
-  const [nextLevel] = useState<number>(scoreProps.nextLevel);
-  const [level] = useState<number>(scoreProps.level);
+const UserLevel = (userLevelProps: UserLevelProps) => {
+  const [nextLevelPercentage] = useState<number>(
+    userLevelProps.nextLevelPercentage
+  );
+  const [level] = useState<number>(userLevelProps.level);
 
   return (
     <Widget>
       <div className='grid grid-cols-2'>
         <div className='h-full w-full content-around'>
           <div className='flex flex-col items-center justify-center border-r-2 border-brand-400 py-12'>
-            <h2 className='text-4xl font-bold uppercase'>Bichinho</h2>
-            <h2 className='text-4xl font-bold uppercase'>
+            <h2 className='text-4xl font-bold'>Bichinho</h2>
+            <h2 className='text-4xl font-bold'>
               Nível <span className='text-brand-500'>{level}</span>
             </h2>
           </div>
         </div>
         <div className='flex items-center justify-center'>
-          <CircularProgress>{nextLevel}</CircularProgress>
+          <CircularProgress
+            nextLevelPercentage={nextLevelPercentage}
+          ></CircularProgress>
         </div>
       </div>
     </Widget>
