ac75c34533c084a85e646ea1ba768e161913acc5
Patolinow <fabiodasn1@gmail.com>
2024-06-22 02:35:45 -0300

    feat: updated input color

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'placeholder:text-muted-background flex h-10 w-full rounded-lg border border-input bg-foreground px-3 py-2 text-sm text-secondary ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'placeholder:text-muted-background flex h-10 w-full rounded-lg border border-input bg-[#3c3634] px-3 py-2 text-sm text-brand-50 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
