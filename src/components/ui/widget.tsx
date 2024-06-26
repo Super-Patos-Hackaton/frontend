34d10a20bd73f993a4fdb3aff3bb7df7e841803f
Patolinow <fabiodasn1@gmail.com>
2024-06-26 03:59:47 -0300

    fix: fixed some sizes of the elements

--- src/components/ui/widget.tsx
+++ src/components/ui/widget.tsx
@@ -2,10 +2,15 @@ import { ReactNode } from 'react';
 
 interface WidgetProps {
   children: ReactNode;
+  className?: string;
 }
 
-const Widget = ({ children }: WidgetProps) => {
-  return <div className='w-[576px] rounded-xl bg-secondary'>{children}</div>;
+const Widget = ({ children, className }: WidgetProps) => {
+  return (
+    <div className={'w-[576px] rounded-xl bg-secondary px-6 py-1 ' + className}>
+      {children}
+    </div>
+  );
 };
 
 export default Widget;
