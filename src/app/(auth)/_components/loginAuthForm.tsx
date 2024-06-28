5a5fc0e509e65eb775215352b6d095fabb66e71b
Patolinow <fabiodasn1@gmail.com>
2024-06-28 04:40:33 -0300

    feat: added error alert component to login auth

--- src/app/(auth)/_components/loginAuthForm.tsx
+++ src/app/(auth)/_components/loginAuthForm.tsx
@@ -2,9 +2,11 @@
 
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { Button } from '@/components/ui/button';
+import { ErrorAlertPortal } from '@/components/ui/error-alert';
 import { Input } from '@/components/ui/input';
 import { Typography } from '@/components/ui/typography';
 
@@ -31,10 +33,14 @@ const loginFormSchema = z.object({
     ),
 });
 
+const errorTitle = 'optional';
+const errorMessage = 'Erro ao efetuar login, tente novamente mais tarde.';
+
 type LoginFormSchema = z.infer<typeof loginFormSchema>;
 
 const LoginAuthForm = () => {
   const router = useRouter();
+  const [isError, setIsError] = useState(false);
 
   const { register, formState, handleSubmit, getValues } =
     useForm<LoginFormSchema>({
@@ -46,9 +52,15 @@ const LoginAuthForm = () => {
   const onSuccess = (response: Response) => {
     router.push('/');
   };
-
   // eslint-disable-next-line no-unused-vars
-  const onError = (error: Error) => {};
+  const onError = (error: Error) => {
+    // sei q deveria puxar o erro daqui mas meu cerebro ja parou de funcionar e vc provavelmente vai ter uma ideia melhor, apaga esse comentário dps
+    setIsError(true);
+    // the timeout is 4001 because the animation takes 4s second to complete
+    setTimeout(() => {
+      setIsError(false);
+    }, 4001);
+  };
 
   const { mutate } = useLoginMutation(onSuccess, onError);
 
@@ -60,55 +72,60 @@ const LoginAuthForm = () => {
   };
 
   return (
-    <form
-      data-testid='login-form'
-      className='m-auto flex w-1/3 flex-col gap-4'
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div>
-        <Input
-          data-testid='email-input-test'
-          type='text'
-          placeholder='Email'
-          className='text-brand-50'
-          {...register('email')}
-        />
-        <ErrorMessage
-          as={Typography.Error}
-          errors={formState.errors}
-          className='text-destructive'
-          name='email'
-        />
-      </div>
-      <div>
-        <Input
-          data-testid='password-input-test'
-          type='password'
-          placeholder='Password'
-          autoComplete='on'
-          {...register('currentPassword')}
-        />
-        <ErrorMessage
-          as={Typography.Error}
-          errors={formState.errors}
-          className='text-destructive'
-          name='currentPassword'
-        />
-        <Link href='/recovery' data-testid='recovery-link-test'>
-          <Typography.Muted className='ml-1 mt-1 text-brand-400 hover:text-brand-500'>
-            Recuperar senha
-          </Typography.Muted>
-        </Link>
-      </div>
-      <Button
-        type='submit'
-        size='lg'
-        data-testid='submit-button-test'
-        className='w-full rounded-lg text-lg'
+    <>
+      {isError && (
+        <ErrorAlertPortal title={errorTitle} message={errorMessage} />
+      )}
+      <form
+        data-testid='login-form'
+        className='m-auto flex w-1/3 flex-col gap-4'
+        onSubmit={handleSubmit(onSubmit)}
       >
-        entrar
-      </Button>
-    </form>
+        <div>
+          <Input
+            data-testid='email-input-test'
+            type='text'
+            placeholder='Email'
+            className='text-brand-50'
+            {...register('email')}
+          />
+          <ErrorMessage
+            as={Typography.Error}
+            errors={formState.errors}
+            className='text-destructive'
+            name='email'
+          />
+        </div>
+        <div>
+          <Input
+            data-testid='password-input-test'
+            type='password'
+            placeholder='Password'
+            autoComplete='on'
+            {...register('currentPassword')}
+          />
+          <ErrorMessage
+            as={Typography.Error}
+            errors={formState.errors}
+            className='text-destructive'
+            name='currentPassword'
+          />
+          <Link href='/recovery' data-testid='recovery-link-test'>
+            <Typography.Muted className='ml-1 mt-1 text-brand-400 hover:text-brand-500'>
+              Recuperar senha
+            </Typography.Muted>
+          </Link>
+        </div>
+        <Button
+          type='submit'
+          size='lg'
+          data-testid='submit-button-test'
+          className='w-full rounded-lg text-lg'
+        >
+          entrar
+        </Button>
+      </form>
+    </>
   );
 };
 
